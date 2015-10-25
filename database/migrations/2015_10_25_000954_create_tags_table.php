23342f3309ea481e0c761ec2e29c8e31de9f3286
Jessica Straw <jcstraw3@gmail.com>
2015-10-25 17:24:32 -0400

    Removes foreign key constraints and old uploads files, adds gender select to user viewUpdate

--- database/migrations/2015_10_25_000954_create_tags_table.php
+++ database/migrations/2015_10_25_000954_create_tags_table.php
@@ -18,10 +18,8 @@ class CreateTagsTable extends Migration {
 
         Schema::create('photo_tag', function (Blueprint $table) {
             $table->integer('photo_id')->unsigned()->index();
-            $table->foreign('photo_id')->references('id')->on('photos')->onDelete('cascade');
 
             $table->integer('tag_id')->unsigned()->index();
-            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
 
             $table->timestamps();
         });
--- resources/views/user/viewUpdate.blade.php
+++ resources/views/user/viewUpdate.blade.php
@@ -9,7 +9,14 @@
 
 			<input name='name' type='text' value='{{ $user->name }}' placeholder='Name'>
 			<input name='email' type='email' value='{{ $user->email }}' placeholder='Email'>
-			<input name='gender' type='text' value='{{ $user->gender }}' placeholder='Gender'>
+			<select class='form-control' name='gender'>
+				@if($user->gender)
+					<option value='{{ $user->gender }}' selected>{{ $user->gender }}</option>
+				@endif
+
+				<option value='Female'>Female</option>
+				<option value='Male'>Male</option>
+			</select>
 			<input name='birthday' type='date' value='{{ $user->birthday }}' placeholder='Birthday'>
 			<textarea name='description' type='text' placeholder='Description'>{{ $user->description }}</textarea>
 
