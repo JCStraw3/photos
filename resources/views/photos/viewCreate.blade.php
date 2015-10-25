2e770c8df1302a262f68b21a48eeca7b8c0386af
Jessica Straw <jcstraw3@gmail.com>
2015-10-25 16:26:21 -0400

    Adds ability to chose privacy setting for photos

--- resources/views/photos/viewCreate.blade.php
+++ resources/views/photos/viewCreate.blade.php
@@ -11,6 +11,10 @@
 		<form action='/photos' method='post' enctype='multipart/form-data'>
 			<input name='title' type='text' placeholder='Title'>
 			<textarea name='description' type='text' placeholder='Description'></textarea>
+			<select name='private'>
+				<option value='0'>Public</option>
+				<option value='1'>Private</option>
+			</select>
 
 			Select image to upload:
 			<input name='image' type='file'>
