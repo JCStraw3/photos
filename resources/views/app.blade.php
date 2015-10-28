786fbe240ccf154cf29b4f97087b903d343df708
Jessica Straw <jcstraw3@gmail.com>
2015-10-27 23:57:00 -0400

    Working on PureCSS menu

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,16 +2,17 @@
 
 <html>
 
-<head>
-	<title>Photos</title>
-</head>
+	<head>
+		<title>Photos</title>
+		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+	</head>
 
-<body>
+	<body>
 
-	@include('partials.nav')
+		@include('partials.nav')
 
-	@yield('content')
+		@yield('content')
 
-</body>
+	</body>
 
 </html>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,8 +1,13 @@
-<div>
+<div class="pure-menu pure-menu-horizontal">
 
-	<a href='/photos'>Photos</a>
+	<a href='/photos' class="pure-menu-heading pure-menu-link">Photos</a>
 
-	<span> | </span>
+	<ul class="pure-menu-list">
+		<li class="pure-menu-item"><a href='/user/{{ $user->id }}' class="pure-menu-link">{{ $user->name }}</a></li>
+		<li class="pure-menu-item"><a href='/auth/logout' class="pure-menu-link">Logout</a></li>
+	</ul>
+
+	{{-- <span> | </span>
 
 	<a href='/photos/create'>Create Photo</a>
 
@@ -32,7 +37,20 @@
 
 	<span> | </span>
 
-	<a href='/auth/logout'>Logout</a>
+	<a href='/auth/logout'>Logout</a> --}}
+
+</div>
+
+<hr />
+
+<div class="pure-menu pure-menu-horizontal">
+
+	<ul class="pure-menu-list">
+		<li class="pure-menu-item"><a href='/photos/create' class="pure-menu-link">Create Photo</a></li>
+		<li class="pure-menu-item"><a href='/tags' class="pure-menu-link">Tags</a></li>
+		<li class="pure-menu-item"><a href='/comments' class="pure-menu-link">Comments</a></li>
+		<li class="pure-menu-item"><a href='/likes' class="pure-menu-link">Likes</a></li>
+	</ul>
 
 </div>
 
