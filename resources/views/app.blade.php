6cbaef223bbc0f330aa391246d69fd7acc1e0cc0
Jessica Straw <jcstraw3@gmail.com>
2015-10-31 21:05:23 -0400

    Adds CSS file to app view file

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
 
 	<head>
 		<title>Photos</title>
+		<link rel='stylesheet' type='text/css' href='/css/app.css'>
 		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	</head>
