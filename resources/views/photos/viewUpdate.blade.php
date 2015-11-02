d6ae6f1d39f81417b58d269cd5812a9284c121f6
Jessica Straw <jcstraw3@gmail.com>
2015-11-02 01:55:18 -0500

    Adds CSS classes to photo update

--- resources/views/photos/viewUpdate.blade.php
+++ resources/views/photos/viewUpdate.blade.php
@@ -8,36 +8,40 @@
 
 	{{-- Form to update a photo --}}
 
-	<div>
+	<div class='card'>
 
 		<h2>Edit Photo</h2>
 
 		<div>
 
-			<img src='/uploads/{{ $photo->image }}'>
+			<div>
+				<img src='/uploads/{{ $photo->image }}' class="pure-img image">
+			</div>
 
-			<form action='/photos/{{ $photo->id }}' method='post' class="pure-form pure-form-stacked">
-				<fieldset>
-					<input name='_method' type='hidden' value='put'>
+			<div class='media-text'>
+				<form action='/photos/{{ $photo->id }}' method='post' class="pure-form pure-form-stacked">
+					<fieldset>
+						<input name='_method' type='hidden' value='put'>
 
-					<input name='title' type='text' value='{{ $photo->title }}' placeholder='Title' class="pure-input-1">
+						<input name='title' type='text' value='{{ $photo->title }}' placeholder='Title' class="pure-input-1">
+						
+						<textarea name='description' type='text' placeholder='Description' class="pure-input-1">{{ $photo->description }}</textarea>
+
+						<select name='tags[]' multiple class="pure-input-1">
+							@foreach($tags as $tag)
+								<option value='{{ $tag->id }}'>{{ $tag->name }}</option>
+							@endforeach
+						</select>
 					
-					<textarea name='description' type='text' placeholder='Description' class="pure-input-1">{{ $photo->description }}</textarea>
-
-					<select name='tags[]' multiple class="pure-input-1">
-						@foreach($tags as $tag)
-							<option value='{{ $tag->id }}'>{{ $tag->name }}</option>
-						@endforeach
-					</select>
-				
-					<select name='private' class="pure-input-1">
-						<option value='0'>Public</option>
-						<option value='1'>Private</option>
-					</select>
-					
-					<button class="pure-button pure-button-primary pure-u-1" type='submit'>Save</button>
-				</fieldset>
-			</form>
+						<select name='private' class="pure-input-1">
+							<option value='0'>Public</option>
+							<option value='1'>Private</option>
+						</select>
+						
+						<button class="pure-button pure-button-primary pure-u-1" type='submit'>Save</button>
+					</fieldset>
+				</form>
+			</div>
 
 		</div>
 
