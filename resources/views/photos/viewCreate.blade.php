3080f03e0719b2b6bd252b87d66d9c10fadd51bb
Jessica Straw <jcstraw3@gmail.com>
2015-11-07 22:29:39 -0500

    Fixes photo create so that it attaches multiple tags

--- resources/views/photos/viewCreate.blade.php
+++ resources/views/photos/viewCreate.blade.php
@@ -14,31 +14,35 @@
 
 		<div class='media-text'>
 
-			<form action='/photos' method='post' enctype='multipart/form-data' class="pure-form pure-form-stacked">
+			<form action='/photos' method='post' enctype='multipart/form-data' class='pure-form pure-form-stacked'>
 				<fieldset>
-					<input name='title' type='text' placeholder='Title' class="pure-input-1">
+					<input name='title' type='text' placeholder='Title' class='pure-input-1'>
 
-					<textarea name='description' type='text' placeholder='Description' class="pure-input-1"></textarea>
+					<textarea name='description' type='text' placeholder='Description' class='pure-input-1'></textarea>
 				
-					<select name='tags' multiple class="pure-input-1">
+					<select name='tags[]' multiple class='pure-input-1'>
 						@foreach($tags as $tag)
 							<option value='{{ $tag->id }}'>{{ $tag->name }}</option>
 						@endforeach
 					</select>
 				
-					<select name='private' class="pure-input-1">
+					<select name='private' class='pure-input-1'>
 						<option value='0'>Public</option>
 						<option value='1'>Private</option>
 					</select>
 				
 					Select image to upload: 
 					<input name='image' type='file'>
-					<input class="pure-button pure-button-primary pure-u-1" name='submit' type='submit' value='Create'>
+					<input class='pure-button pure-button-primary pure-u-1' name='submit' type='submit' value='Create'>
 				</fieldset>
 			</form>
 
 		</div>
 
 	</div>
+
+	{{-- Select2 scripts --}}
+
+
 
 @endsection
