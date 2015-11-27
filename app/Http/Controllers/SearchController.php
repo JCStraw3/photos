5804816d263463ca482547134912dccf09b952e0
Jessica Straw <jcstraw3@gmail.com>
2015-11-27 17:14:21 -0500

    Commit

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use App\Photo;
+
 class SearchController extends Controller {
 
 	public function actionSearch(Request $request){
