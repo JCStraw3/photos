87614e57cac4fb30172154740bc831bccb4ad9b5
Jessica Straw <jcstraw3@gmail.com>
2015-12-17 23:49:03 -0500

    Adds footer

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,6 +21,8 @@
 
 		@yield('content')
 
+		@include('partials.footer')
+
 	</body>
 
 </html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer>
+	<div>
+		<p>
+			© Copyright <a href='http://jessicastraw.com/'>Jessica Straw</a>
+		</p>
+	</div>
+</footer>
